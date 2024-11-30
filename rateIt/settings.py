c03db78640636f3d8384ebcaa37c25e783255431
Ali Elahi <a.elahi@mahsan.co>
2024-11-30 17:51:03 +0330

    feat: Create the post django app

--- rateIt/settings.py
+++ rateIt/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # other apps...
+    'rest_framework',
+    'post',
 ]
 
 MIDDLEWARE = [
